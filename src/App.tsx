3e5ede925680215d2dd578114f43c2fd745947da
Aidar Ibraev <aidaribraev32@gmail.com>
2023-04-21 12:55:24 +0600

    breaks the project down into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img src="../../logo192.png"/>
-            </header>
-            <nav className="nav">
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className="content">
-                <img className="imgCity" src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80"/>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        post1
-                    </div>
-                    <div>
-                        post2
-                    </div>
-                </div>
-            </div>
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <img className="imgCity" src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2l0eXxlbnwwfHwwfHw%3D&w=1000&q=80"/>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Profile;
