5c0730bad1dee36a3a2229f5f38403ba18af201a
Aidar Ibraev <aidaribraev32@gmail.com>
2023-05-03 20:20:47 +0600

    Commit - 08. Added new components (DialogItem.tsx and Message.tsx). Change <Route component> => <Route render> in App.tsx

---
+++ src/components/Dialogs/Message/Message.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from "../Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+
+type MessagePropsType = {
+    message: string
+}
+
+const Message = (props: MessagePropsType) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+}
+
+export default Message;
